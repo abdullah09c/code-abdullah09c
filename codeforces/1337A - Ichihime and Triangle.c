b67488825a7d0c634a418827614c4c2701e75f67
Abdullah Al Fuwad <138917560+abdullah09c@users.noreply.github.com>
2023-07-12 11:37:37 +0600

    Update 1337A - Ichihime and Triangle.c
    In Triangle a+b > c

--- codeforces/1337A - Ichihime and Triangle.c
+++ codeforces/1337A - Ichihime and Triangle.c
@@ -4,11 +4,12 @@ int main()
   int t, a,b,c,d;
   scanf("%d", &t);
   while(t--) {
-    scanf("%d%d%d%d", &a, &b, &c, &d);		if(a+b >= c) {
+    scanf("%d%d%d%d", &a, &b, &c, &d);	
+    if(a+b > c) {
     printf("%d %d %d\n", a, b, c);
     continue;
     }
-    else if(b+c >= d) {
+    else if(b+c > d) {
     printf("%d %d %d\n", b, c, d);
     continue;
     }
