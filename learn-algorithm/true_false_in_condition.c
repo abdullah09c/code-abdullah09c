496663be205767b5d2e3d7458a3452cd907577b9
Abdullah Al Fuwad <138917560+abdullah09c@users.noreply.github.com>
2023-07-12 23:41:02 +0600

    Create true_false_in_condition.c

---
+++ learn-algorithm/true_false_in_condition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	
+	int a = 10;
+	if(a)// greater than and equal 1 is true
+	printf("True_Ok\n");
+	
+	a = 0;
+	
+	if(!a)// it means if a is false the code inside condition will execute. In computer 0 = false.
+	printf("False_Ok");
+	
+	return 0;
+}
