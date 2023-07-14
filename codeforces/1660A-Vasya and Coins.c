51a68aaae48eaec0292fa4cd59884e2eff193e36
Abdullah Al Fuwad <138917560+abdullah09c@users.noreply.github.com>
2023-07-14 19:34:06 +0600

    Update 1660A-Vasya and Coins.c

--- codeforces/1660A-Vasya and Coins.c
+++ codeforces/1660A-Vasya and Coins.c
@@ -5,10 +5,8 @@ int main()
   scanf("%d", &t);
   while(t--) {
     scanf("%d%d", &a, &b);
-    if(a == 0 || (a == 0 && b == 0))
+    if(!a)
     printf("1\n");
-    else if(b == 0)
-    printf("%d\n", a + 1);
     else
     printf("%d\n", (a + 2*b) + 1);
     
