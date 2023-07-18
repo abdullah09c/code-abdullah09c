8df219db331cebcebafd856b6fff01b016987d41
Abdullah Al Fuwad <138917560+abdullah09c@users.noreply.github.com>
2023-07-18 18:13:41 +0600

    Update 1527A And Then There Were K.c

--- codeforces/1527A And Then There Were K.c
+++ codeforces/1527A And Then There Were K.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 int main()
 {
-  int i, n, t;
+  int i, n, t, s = 0;
   scanf("%d", &t);
   while(t--) {
     scanf("%d", &n);
-    for(i = n; i > 0; i--) {
-      if((i & i - 1) == 0) {
-        printf("%d\n", i - 1);
+    if(n == 1)
+    printf("0\n");
+    
+    else {
+    
+    for(i = 0; ; i = s) {
+      
+      s = 2*i + 1;
+      if(n <= 2*s + 1) {
+        printf("%d\n", s);
         break;
       }
     }
+    
+   } 
+    
   }
   
   return 0;
