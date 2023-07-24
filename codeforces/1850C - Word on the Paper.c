46df3822f33fefa0e5340051c1ba1a9b7d50787c
Abdullah Al Fuwad <138917560+abdullah09c@users.noreply.github.com>
2023-07-24 13:13:34 +0600

    version_1.2 1850C - Word on the Paper.c
    no need name[] string

--- codeforces/1850C - Word on the Paper.c
+++ codeforces/1850C - Word on the Paper.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 int main()
 {
-   char str[8][8];
-   int i, j, t;
+   char str[10];
+   int t, i;
    scanf("%d", &t);
    while (t--)
    {
-    int k = 0;
-    for ( i = 0; i < 8; i++)
+    int j = 1;
+    while (j <= 8)
     {
-        scanf("%s", str[i]);
-        for ( j = 0; j < 8; j++)
-        {
-          if(str[i][j] != '.'){
-            printf("%c", str[i][j]);
-          }
-        }
-        
+     
+     scanf("%s", str);
+     
+     for ( i = 0; str[i] != '\0'; i++)
+     { 
+      if(str[i] != '.') {
+        printf("%c", str[i]);
+      }
+
+     }
+     j++; 
     }
-        printf("\n");
+    printf("\n");
    }
-   
 
    return 0;
 }
