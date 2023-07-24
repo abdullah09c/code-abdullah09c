1c658b1c72306ac62ac49b66b3b7717bf9d3893c
Abdullah Al Fuwad <138917560+abdullah09c@users.noreply.github.com>
2023-07-24 13:17:03 +0600

    version_1.0 1850C - Word on the Paper.c

--- codeforces/1850C - Word on the Paper.c
+++ codeforces/1850C - Word on the Paper.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-   char str[10];
+   char str[10], name[10];
    int t, i;
    scanf("%d", &t);
    while (t--)
    {
-    int j = 1;
+    int j = 1, k = 0;
     while (j <= 8)
     {
      
@@ -15,13 +15,17 @@ int main()
      for ( i = 0; str[i] != '\0'; i++)
      { 
       if(str[i] != '.') {
-        printf("%c", str[i]);
+        name[k] = str[i];
+        k++;
+        break;
       }
 
      }
      j++; 
     }
-    printf("\n");
+    name[k] = '\0';
+    printf("%s\n", name);
+    
    }
 
    return 0;
