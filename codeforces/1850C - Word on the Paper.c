b3031f3a6018c8f53c9def233f91d0685b55a47b
Abdullah Al Fuwad <138917560+abdullah09c@users.noreply.github.com>
2023-07-24 13:10:26 +0600

    version_2.0 1850C - Word on the Paper.c
    2d strings

--- codeforces/1850C - Word on the Paper.c
+++ codeforces/1850C - Word on the Paper.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 int main()
 {
-   char str[10], name[10];
-   int t, i;
+   char str[8][8];
+   char name[8];
+   int i, j, t;
    scanf("%d", &t);
    while (t--)
    {
-    int j = 1, k = 0;
-    while (j <= 8)
+    int k = 0;
+    for ( i = 0; i < 8; i++)
     {
-     
-     scanf("%s", str);
-     
-     for ( i = 0; str[i] != '\0'; i++)
-     { 
-      if(str[i] != '.') {
-        name[k] = str[i];
-        k++;
-        break;
-      }
-
-     }
-     j++; 
+        scanf("%s", str[i]);
+        for ( j = 0; j < 8; j++)
+        {
+          if(str[i][j] != '.'){
+            name[k] = str[i][j];
+            k++;
+          }
+        }
+        
     }
     name[k] = '\0';
     printf("%s\n", name);
-    
+
    }
+   
 
    return 0;
 }
